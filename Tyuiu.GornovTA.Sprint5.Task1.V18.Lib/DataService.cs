528839c35e60d15fd93d9c18e1bff02cea8f7293
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-11-15 17:53:46 +0500

    Task1

---
+++ Tyuiu.GornovTA.Sprint5.Task1.V18.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.GornovTA.Sprint5.Task1.V18.Lib
+{
+    public class DataService : ISprint5Task1V18
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(3 * x + 2 - ((2 * x - x) / (Math.Cos(x) + 1)), 2);
+                strY = Convert.ToString(y);
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            return path;
+        }
+    }
+}
