b33d28b0da572f2aea5aad4ccbbfae290198b94d
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-11-22 17:12:37 +0500

    Task4

---
+++ Tyuiu.GornovTA.Sprint5.Task4.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Tyuiu.GornovTA.Sprint5.Task4.V1.Lib;
+
+namespace Tyuiu.GornovTA.Sprint5.Task4.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №5 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла.                                *");
+            Console.WriteLine("* Задание №4                                                              *");
+            Console.WriteLine("* Вариант №1                                                              *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V1.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
